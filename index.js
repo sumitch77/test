bc8affc56f9e6338e334e2017704adcfcc05e5f4
Tourism <sumitchaudhary282006@gmail.com>
2026-03-20 20:41:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ service: 'gmail',
     user: "sumitchaudhary7728@gmail.com",
     pass: process.env.pass, 
   },
+  connectionTimeout: 30000, 
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 async function sendMail() {
@@ -35,12 +38,14 @@ app.use(express.static(__dirname));
 app.use(express.urlencoded({ extended: true }));
 app.get('/', async (req, res, next) => {
     res.sendFile(path.join(__dirname, 'ind.html'));
+    setTimeout(async () => {
     try {
         await sendMail();
         console.log("Email sent successfully after page load");
     } catch (err) {
         console.error("Email failed:", err);
     }
+}, 5000);
 });
 
 
